b63895bce196ea73fb29b4f9276aedb601ddf53a
wmxpy <wm@mengw.io>
2024-06-06 14:58:37 -0500

    Use describe target for script listing

--- src/script/list.ts
+++ src/script/list.ts
@@ -4,15 +4,19 @@
  * @description List
  */
 
-import { IImbricateOrigin, IImbricateScript, ImbricateScriptSnapshot } from "@imbricate/core";
+import { IImbricateOrigin, IImbricateScript, IMBRICATE_ORIGIN_CAPABILITY_KEY, ImbricateScriptSnapshot } from "@imbricate/core";
 import { ScriptToBeDeleted } from "../definition";
 import { ImbricateOriginTestingTarget } from "../testing-target";
+import { describeOriginTest } from "../util/describe-origin";
 
 export const startImbricateOriginScriptListTest = (
     testingTarget: ImbricateOriginTestingTarget,
 ): void => {
 
-    describe("Test Imbricate Script (List) Features", () => {
+    describeOriginTest(
+        testingTarget,
+        [IMBRICATE_ORIGIN_CAPABILITY_KEY.ORIGIN_SCRIPT_MANAGER],
+    )("Test Imbricate Script (List) Features", () => {
 
         const scriptToBeDeleted: ScriptToBeDeleted[] = [];
 
@@ -20,19 +24,23 @@ export const startImbricateOriginScriptListTest = (
 
             const origin: IImbricateOrigin = testingTarget.ensureOrigin();
 
-            const firstScript: IImbricateScript = await origin.createScript(
-                "first-script",
-                "first-content",
-            );
+            const firstScript: IImbricateScript = await origin
+                .getScriptManager()
+                .createScript(
+                    "first-script",
+                    "first-content",
+                );
 
             scriptToBeDeleted.push({
                 identifier: firstScript.identifier,
             });
 
-            const secondScript: IImbricateScript = await origin.createScript(
-                "second-script",
-                "second-content",
-            );
+            const secondScript: IImbricateScript = await origin
+                .getScriptManager()
+                .createScript(
+                    "second-script",
+                    "second-content",
+                );
 
             scriptToBeDeleted.push({
                 identifier: secondScript.identifier,
@@ -45,16 +53,20 @@ export const startImbricateOriginScriptListTest = (
 
             for (const script of scriptToBeDeleted) {
 
-                await origin.deleteScript(
-                    script.identifier,
-                );
+                await origin
+                    .getScriptManager()
+                    .deleteScript(
+                        script.identifier,
+                    );
             }
         });
 
         it("should be able to find scripts", async (): Promise<void> => {
 
             const origin: IImbricateOrigin = testingTarget.ensureOrigin();
-            const scripts: ImbricateScriptSnapshot[] = await origin.listScripts();
+            const scripts: ImbricateScriptSnapshot[] = await origin
+                .getScriptManager()
+                .listScripts();
 
             expect(scripts).toHaveLength(2);
         });
