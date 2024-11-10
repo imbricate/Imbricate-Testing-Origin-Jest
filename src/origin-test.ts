7d15ec623d38742a89d7e513f5029943042ff2dd
wmxpy <wm@mengw.io>
2024-11-10 17:24:06 -0600

    chore: update @imbricate/core dependency to local path and remove unused script and page tests

--- src/origin-test.ts
+++ src/origin-test.ts
@@ -8,17 +8,6 @@ import { startImbricateOriginCollectionCreateTest } from "./collection/create";
 import { startImbricateOriginCollectionFindTest } from "./collection/find";
 import { startImbricateOriginCollectionRenameTest } from "./collection/rename";
 import { describeImbricateOriginTest } from "./describe-test";
-import { startImbricateOriginPageAttributeTest } from "./page/attribute";
-import { startImbricateOriginPageCreateTest } from "./page/create";
-import { startImbricateOriginPageListTest } from "./page/list";
-import { startImbricateOriginPageRetitleTest } from "./page/retitle";
-import { startImbricateOriginPageSearchTest } from "./page/search";
-import { startImbricateOriginPageUpdateTest } from "./page/update";
-import { startImbricateOriginScriptAttributeTest } from "./script/attribute";
-import { startImbricateOriginScriptCreateTest } from "./script/create";
-import { startImbricateOriginScriptListTest } from "./script/list";
-import { startImbricateOriginScriptRenameTest } from "./script/rename";
-import { startImbricateOriginScriptSearchTest } from "./script/search";
 import { ImbricateOriginTestingTarget } from "./testing-target";
 
 export const startImbricateOriginTest = (
@@ -33,19 +22,6 @@ export const startImbricateOriginTest = (
             startImbricateOriginCollectionCreateTest(testingTarget);
             startImbricateOriginCollectionFindTest(testingTarget);
             startImbricateOriginCollectionRenameTest(testingTarget);
-
-            startImbricateOriginPageAttributeTest(testingTarget);
-            startImbricateOriginPageCreateTest(testingTarget);
-            startImbricateOriginPageListTest(testingTarget);
-            startImbricateOriginPageRetitleTest(testingTarget);
-            startImbricateOriginPageSearchTest(testingTarget);
-            startImbricateOriginPageUpdateTest(testingTarget);
-
-            startImbricateOriginScriptAttributeTest(testingTarget);
-            startImbricateOriginScriptCreateTest(testingTarget);
-            startImbricateOriginScriptListTest(testingTarget);
-            startImbricateOriginScriptRenameTest(testingTarget);
-            startImbricateOriginScriptSearchTest(testingTarget);
         },
     );
 };
